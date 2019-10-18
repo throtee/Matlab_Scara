b2be6966c6b12d55faf1487f72c5a3a600fc6ff9
throtee <55964537+throtee@users.noreply.github.com>
2019-10-18 15:50:57 +0800

    Final version
    still can be improved. it looks clean and functionable so it wouldnt be changed unless error happens.

--- inverse_kinematics.m
+++ inverse_kinematics.m
@@ -1,47 +1,37 @@
-function [q1,q2,q3,q4]=inverse_kinematics(T)
-% syms q1_J q2_J q3_J q4_J
-syms q1_C q2_C q3_C q4_C
-syms q1 q2 q3 q4
+%the function is used to input the homogeneous transformation matrix of SCARA and get
+%the D-H variables of 'goal' position
+%Made by: Enze Chen 3072167875@qq.com
+%inspired by:Niels Joubert http://www-inst.cs.berkeley.edu/~cs184/fa09/resources/ik_soln.pdf
 
-% q1_C=0;q2_C=0;q3_C=0;q4_C=0;
-
-q_sum=[q1,q2,q3,q4]';
-q_sum_C=[q1_C,q2_C,q3_C,q4_C]';
-
-% p=T*p_0;
-% 
-% T_sum=T1*T2*T3*T4;
-
-T_IK=T_maker(q1,q2,q3,q4);
-T_sum_C=T_maker(q1_C,q2_C,q3_C,q4_C);
-
-
-% T_result=[T_TK(11),T_TK(21),T_TK(12),T_TK(22),T_TK(32),T_TK(13),T_TK(23),T_TK(33),T_TK(14),T_TK(24),T_TK(34)]';
-
-Jacobian_T=jacobian([T_IK(11),T_IK(21),T_IK(12),T_IK(22),T_IK(32),T_IK(13),T_IK(23),T_IK(33),T_IK(14),T_IK(24),T_IK(34)],[q1,q2,q3,q4]);
-
-T_result=T_sum_C+Jacobian_T*(q_sum-q_sum_C);
-
-
-
-
-
-end
-
-
-function T=T_maker(q1,q2,q3,q4)
+function [q1,q2,q3,q4]=inverse_kinematics(T,I11,I22,I33,I44)
 
 %set the variables
 
-%use Link command to create link
-L1=Link([q1 0 40 0 0]);
-L2=Link([q2 0 40 0 0]);
-L3=Link([0 q3 0 0 1 ]);
-L4=Link([q4 20 0 0 0]);
+%define as 'current' D-H variables
+syms q1_C q2_C q3_C q4_C
+%define as storage of 'current' D-H variables value, default is 0
+syms I1 I2 I3 I4
+switch nargin
+    case 1
+        I1=10; I2=10; I3=10; I4=0;
+    otherwise
+        I1=I11; I2=I22; I3=I33; I4=I44;
+end
+%----------------------------------------------------------------
+
+%construct the SCARA matrix
+
+%create link
+L1=[q1_C 0 40 0];
+L2=[q2_C 0 40 0];
+L3=[0 q3_C 0 0 ];
+L4=[q4_C 20 0 0];
 L_SCARA=[L1;L2;L3;L4];
 
-T=eye(4);
+%generate the default homogeneous transformation matrix
+T_sum=eye(4);
 
+%create the homogeneous transformation matrix
 for i=1:4
     theta_tem=L_SCARA(i,1);
     d_tem=L_SCARA(i,2);
@@ -53,13 +43,75 @@ for i=1:4
         0,sin(alpha_tem),cos(alpha_tem),d_tem;
         0,0,0,1];
     
-     %sum_A(:,i)=A_tem;
- 
-    T=T*A_tem;
+    T_sum=T_sum*A_tem;
+end
+
+%--------------------------------------------------------------------------------------
+
+%transform the jacobian matrix,homogeneous transformation
+%matrix, homogeneous transformation matrix of the goal position to fit the
+%calculation format of q_delta
+Jacobian_T=jacobian([T_sum(1,1),T_sum(2,1),T_sum(1,2),T_sum(2,2), ...
+    T_sum(3,2),T_sum(1,3),T_sum(2,3),T_sum(3,3),T_sum(1,4), ...
+    T_sum(2,4),T_sum(3,4)],[q1_C,q2_C,q3_C,q4_C]);
+T_sum1=[T_sum(1,1),T_sum(2,1),T_sum(1,2),T_sum(2,2),T_sum(3,2), ...
+    T_sum(1,3),T_sum(2,3),T_sum(3,3),T_sum(1,4),T_sum(2,4),T_sum(3,4)]';
+T1=[T(1,1),T(2,1),T(1,2),T(2,2),T(3,2),T(1,3),T(2,3),T(3,3),T(1,4),T(2,4),T(3,4)]';
+
+%-----------------------------------------------------------------
+
+%IT SHOULD BUILD IN AS A ROTATION, IT IS THE RIGHT WAY MATHMATICALLY 
+%solve and get the D-H variables of 'goal' position
+
+%give the D-H variables the storage value
+q1_C=I1;q2_C=I2;q3_C=I3;q4_C=I4;
+
+%transform the value to the jacobian matrix and inverse it
+%note: SVD is not used, it could be used for learning purposes but my
+%original 'invert_SVD' function has some error unfixed, it is shown below.
+Jacobian_T_C=double(subs(Jacobian_T));
+Jacobian_Inverted_C=invert_SVD(Jacobian_T_C);
+T_sum1_C=subs(T_sum1);
+q_delta=Jacobian_Inverted_C*(T1-T_sum1_C);
+
+% if i=1:100
+% end
+
+%loop for iteration to reduce the distance between goal position and
+%current position, you can get a more accurate value by set the loop for a
+%fixed amount of times
+%it may not converge, when it happens half the 'q_delta' and try again
+while q_delta~=0
+q_delta=Jacobian_Inverted_C*(T1-T_sum1_C);
+q1_C=double(q1_C+q_delta(1)); q2_C=double(q2_C+q_delta(2)); q3_C=double(q3_C+q_delta(3)); q4_C=double(q4_C+q_delta(4));
+
+Jacobian_T_C=double(subs(Jacobian_T));
+Jacobian_Inverted_C=invert_SVD(Jacobian_T_C);
+T_sum1_C=subs(T_sum1);
+end
+
+    
+%------------------------------------------------------------------
+
+q1=double(q1_C);q2=double(q2_C);q3=double(q3_C);q4=double(q4_C);
+q1=wrapToPi(q1); q2=wrapToPi(q2); q4=wrapToPi(q4);
 end
 
 
+% function T=invert_SVD(A)
+% [U,S,V] = svd(A);
+% %get S_plus-----------------------------------------------------------
+% S_plus=S;
+% S_1=S~=0;
+% S_2=1./S(S_1);
+% S_plus(S_1)=S_2;
+% %--------------------------------------------------------------------
+% T=V'*S_plus'*U;
+% end
+function T=invert_SVD(A)
+T=pinv(A);
 end
+
 
 % function [q1,q2,q3,q4]=T_converge(T)
 % end
